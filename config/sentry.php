9d4f3820ddd324a9a56f6a165e5675750a4c9236
Chris Bautista <chrisbjr@gmail.com>
2015-10-09 22:28:20 +0800

    Gave more detailed description of the config

--- config/sentry.php
+++ config/sentry.php
@@ -2,8 +2,32 @@
 
 return [
 
-    'enabled' => true,
+    /*
+	|--------------------------------------------------------------------------
+	| Sentry switch
+	|--------------------------------------------------------------------------
+	|
+	| Set the following value to true to enable logging via Sentry. Of course,
+    | you'll have to set your DSN as well. Usually, we put the followig switch
+    | along with the environment variables. Like so:
+    |
+    | 'enabled' => env('SENTRY_ENABLED', false)
+	|
+	*/
+    'enabled' => false,
 
-    'dsn'     => 'https://***:***@sentry.yourdomain.com/{project}'
+    /*
+	|--------------------------------------------------------------------------
+	| DSN
+	|--------------------------------------------------------------------------
+	|
+	| In your Sentry dashboard, create a new project and grab your DNS from the
+    | settings. Usually, this can be found under Settings -> Client Keys
+    |
+    | The format of the DSN is usally like this:
+    | https://***:***@sentry.yourdomain.com/{project}
+	|
+	*/
+    'dsn'     => ''
 
 ];
